9c5e88375d83cf0de71baf30c81018b136cf5d3c
traveler123456 <2252583743@qq.com>
2020-03-23 19:39:49 +0800

    nacos

---
+++ cloud2020/cloud-alibaba-provider-payment9002/src/main/java/springcloud/ProviderMain9002.java
@@ -0,0 +1,11 @@
+package springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProviderMain9002 {
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderMain9002.class);
+    }
+}
